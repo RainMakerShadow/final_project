3bf3b926430bdaf54ba9d405c1e58428ecb7f256
Григорий <nechay2112@gmail.com>
2023-07-11 02:21:32 +0300

    Improvement of product image saving and output for editing

--- resources/views/products/edit-products.blade.php
+++ resources/views/products/edit-products.blade.php
@@ -73,17 +73,18 @@
                     <label for="img_alt" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ALT-атрибут</label>
                     <input wire:model="img_alt" type="text" id="img_alt" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" value="{{$product->img_alt}}">
                 </div>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Зберегти</button>
             </div>
 
-            <div class="relative m:auto">
+
+            <div class="max-w-lg">
                 @if (is_object($image))
-                    <img class="h-auto max-w-lg rounded-lg" src= "{{$image->temporaryUrl()}}"  alt="image description" width="50%">
+                    <img class="h-auto max-w-xl rounded-lg shadow-xl dark:shadow-gray-800 mt-4 ml-20" src= "{{$image->temporaryUrl()}}"  alt="image description" width="50%">
                 @else
-                    <img class="h-auto max-w-lg rounded-lg" src= "{{$imageUrl}}"  alt="image description" width="50%">
+                    <img class="h-auto max-w-xl rounded-lg shadow-xl dark:shadow-gray-800 mt-4 ml-20" src= "{{$imageUrl}}"  alt="image description" width="50%">
                 @endif
             </div>
         </div>
 
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Зберегти</button>
     </form>
 </div>
